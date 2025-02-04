9642d8224f9866b8081ff0e163e89717be768ce0
nlojas <nlojas@paymentus.com>
2025-02-04 10:01:59 -0500

    Add util script to setup nvim

---
+++ util-scripts/setup-nvim-on-ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y build-essential curl wget python3 make unzip zip ripgrep
+
+# install nvm
+if ! nvm ls; then
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+	source ~/.bashrc
+fi
