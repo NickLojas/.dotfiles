c67100d6aaa9cdec4d748d00f1deb7f48373d064
nlojas <nlojas@paymentus.com>
2025-02-04 09:47:35 -0500

    tsserver is renamed to ts_ls

--- nvim/init.lua
+++ nvim/init.lua
@@ -638,7 +638,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         -- eslint = {
         --   on_attach = function(client, bufnr)
         --     vim.api.nvim_create_autocmd('BufWritePre', {
